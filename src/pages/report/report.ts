d32b8f9e732dc22904ff2d801dbd6cb0c3466939
webmasterdragon101 <webmasterdragon101@gmail.com>
2017-10-29 00:48:01 +0700

    sender,receiver transaction

--- src/pages/report/report.ts
+++ src/pages/report/report.ts
@@ -1,5 +1,11 @@
+import * as childProcess from 'child_process';
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController  } from 'ionic-angular';
+import { AngularFireDatabase } from 'angularfire2/database';
+import { FirebaseProvider } from '../../providers/firebase/firebase';
+import firebase from 'firebase';
+import { Http } from '@angular/http';
+
 
 /**
  * Generated class for the ReportPage page.
@@ -15,13 +21,120 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ReportPage {
   public user:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public transactions:any;
+  public transaction = {
+    avatar:[],
+    name:[],
+    date:[],
+    amount:[],
+  };
+  public transactiontotalmoney:number;
+  constructor(
+    public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public http : Http,
+    public afd: AngularFireDatabase,
+    public firebaseProvider: FirebaseProvider,
+  ) {
     this.user = navParams.get("user");
+    this.http = http;
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ReportPage');
-    console.log(this.user.role);
+    var that = this;
+    that.transactions = [];
+    that.transactiontotalmoney = 0;
+    var query = firebase.database().ref("transactions").orderByKey();
+    query.once("value").then(function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        var transaction = {
+          avatar:[],
+          name:[],
+          date:[],
+          amount:[],
+        }
+        if(that.user.role == 0){
+          if (childSnapshot.val().receiverid == that.user.id && childSnapshot.val().state == 1){
+            var senderid = childSnapshot.val().senderid;
+            var query = firebase.database().ref("users").orderByKey();
+            query.once("value").then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                if (childSnapshot.val().id == senderid ) {
+                  transaction.avatar.push(childSnapshot.val().avatar);
+                  transaction.name.push(childSnapshot.val().fullname);
+                }
+                });
+            });
+            transaction.date.push(childSnapshot.val().transactionid);
+            transaction.amount.push(childSnapshot.val().sendmoney);
+            console.log(transaction);
+            that.transactions.push(transaction);
+            console.log(that.transactions);
+            that.transactiontotalmoney += Number(childSnapshot.val().sendmoney);
+          }
+        }else{
+          if (childSnapshot.val().senderid == that.user.id && childSnapshot.val().state == 1){
+            var receiverid = childSnapshot.val().receiverid;
+            query = firebase.database().ref("users").orderByKey();
+            query.once("value").then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                if (childSnapshot.val().id == receiverid ) {
+                  transaction.avatar.push(childSnapshot.val().avatar);
+                  transaction.name.push(childSnapshot.val().fullname);
+                }
+                });
+            });
+            transaction.date.push(childSnapshot.val().transactionid);
+            transaction.amount.push(childSnapshot.val().sendmoney);
+            console.log(transaction);
+            that.transactions.push(transaction);
+            console.log(that.transactions);
+            that.transactiontotalmoney += Number(childSnapshot.val().sendmoney);
+          }
+        }
+      });
+    });
   }
-
+  deleteTransaction(id){
+    console.log(id);
+    var that = this;
+    var query = firebase.database().ref("transactions").orderByKey();
+    query.once("value").then(function (snapshot) {
+      snapshot.forEach(function (childSnapshot) {
+        if(childSnapshot.val().transactionid == id){
+          // if(that.showConfirm()){
+            childSnapshot.ref.remove();
+            that.navCtrl.push(ReportPage, {
+              user:that.user
+            });
+          }
+        // }
+      });
+    });
+  }
+  // showConfirm() {
+  //   let confirm = this.alertCtrl.create({
+  //     title: 'Are you sure?',
+  //     message: 'Do you agree to delete this transaction?',
+  //     buttons: [
+  //       {
+  //         text: 'Disagree',
+  //         handler: () => {
+  //           console.log('Disagree clicked');
+  //           return true;
+  //         }
+  //       },
+  //       {
+  //         text: 'Agree',
+  //         handler: () => {
+  //           console.log('Agree clicked');
+  //           return true;
+  //         }
+  //       }
+  //     ]
+  //   });
+  //   confirm.present();
+  // }
 }
